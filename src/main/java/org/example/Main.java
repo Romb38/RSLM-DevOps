4e1e8d9775d12ede093ad597275885c1b512c11a
Mathieu Zussy <mathieu.zussy@etu.univ-grenoble-alpes.fr>
2024-04-03 21:08:23 +0200

    Initialisation

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,9 @@
 package org.example;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.Scanner;
+
 public class Main {
 
     /**
@@ -9,5 +13,29 @@ public class Main {
      */
     public static void main(String[] args) {
         System.out.println("Hello world! And root");
+        System.out.println("Enter your username: ");
+        Scanner scanner = new Scanner(System.in);
+        String username = scanner.nextLine();
+        ObjectMapper objectMapper = new ObjectMapper();
+//        Object[] values = objectMapper.
+        System.out.println("Your username is " + username);
+        scanner.close();
+    }
+
+    class Dataframe {
+        private Object[] val;
+        Dataframe(Object[] val) {
+            this.val = val;
+        }
+
+        public String toString() {
+            StringBuilder builder = new StringBuilder();
+            if (this.val != null){
+                for(Object value : this.val){
+                    builder.append(value.toString());
+                }
+            }
+            return builder.toString();
+        }
     }
 }
