baf5a3721aaf2783a17b15adc5b6184ceb4c261a
barbiero <r.barbier38@gmail.com>
2024-04-03 08:59:32 +0200

    Adding Website

---
+++ src/test/java/UnitTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class UnitTest {
+    @Test
+    public void testEquality() {
+        // Assertion pour vérifier si 1 est égal à 1
+        assertEquals(1, 1);
+    }
+}
