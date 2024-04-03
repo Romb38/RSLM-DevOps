c334acd899c1c7f899b575c3bf5935dc16d3cb12
barbiero <r.barbier38@gmail.com>
2024-04-03 10:08:46 +0200

    Adding docs

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,12 @@
 package org.example;
 
 public class Main {
+
+    /**
+     * @brief Main method
+     *
+     * @details This method is the entry point of the program
+     */
     public static void main(String[] args) {
         System.out.println("Hello world! And root");
     }
