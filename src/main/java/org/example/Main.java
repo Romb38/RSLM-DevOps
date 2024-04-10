7e810fe5994331c359b8b0941ef4cf165048d722
mathieu.zussy <mathieu.zussy.pro@gmail.com>
2024-04-11 00:38:02 +0200

    Partial display by column done;

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,13 +7,12 @@ public class Main {
 
     /**
      * @brief Méthode principale
-     *
      * @details This method is the entry point of the program
      */
     public static void main(String[] args) throws Exception {
         // WORK IN PROGRESS
-//            String csvFilePath = System.getenv("CSV_FILE_PATH");
-        DataFrame<String, Integer> dataFrame = new DataFrame<>(Paths.get("C:\\Users\\Jack Asmens\\Documents\\0. Important\\Cours_M1\\input.csv"), ';', String.class, Integer.class);
+        String csvFilePath = System.getenv("CSV_FILE_PATH");
+        DataFrame<String, Integer> dataFrame = new DataFrame<>(Paths.get(csvFilePath), ';', String.class, Integer.class);
         System.out.println("Result: ");
         System.out.println(dataFrame.toStringDisplay());
         System.out.println("Result - last 2: ");
@@ -21,8 +20,8 @@ public class Main {
         System.out.println("Result - first 2: ");
         System.out.println(dataFrame.toStringPartialDisplay(2, true));
         System.out.println("Result - column a & c: ");
-        System.out.println(dataFrame.toStringDisplayByHeader(Arrays.asList("a","c")));
-        Integer[] indexes = {1,2,3,4};
+        System.out.println(dataFrame.toStringDisplayByHeader(Arrays.asList("a", "c")));
+        Integer[] indexes = {1, 2, 3, 4};
         dataFrame.setIndexes(indexes);
         System.out.println("Result with indexes setup: ");
         System.out.println(dataFrame.toStringDisplay());
@@ -33,8 +32,8 @@ public class Main {
         System.out.println("Result with indexes setup - first 2: ");
         System.out.println(dataFrame.toStringPartialDisplay(2, true));
         System.out.println("Result with indexes setup - column a & c: ");
-        System.out.println(dataFrame.toStringDisplayByHeader(Arrays.asList("a","c")));
-        Integer[] indexesFilter = {1,3};
+        System.out.println(dataFrame.toStringDisplayByHeader(Arrays.asList("a", "c")));
+        Integer[] indexesFilter = {1, 3};
         System.out.println("Result - with index 1 & 3: ");
         System.out.println(dataFrame.toStringDisplayByIndex(indexesFilter));
     }
